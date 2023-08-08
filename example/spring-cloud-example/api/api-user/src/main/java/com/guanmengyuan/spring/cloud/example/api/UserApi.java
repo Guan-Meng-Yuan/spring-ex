2cbac92a97238d5fa92de0856b5a63c8b810c2f9
guanmengyuan <GMY.i@icloud.com>
2023-08-08 14:22:16 +0800

    remove example

--- example/spring-cloud-example/api/api-user/src/main/java/com/guanmengyuan/spring/cloud/example/api/UserApi.java
+++
@@ -1,7 +0,0 @@
-package com.guanmengyuan.spring.cloud.example.api;
-
-import com.guanmengyuan.spring.cloud.example.model.domain.User;
-import com.mybatisflex.core.service.IService;
-
-public interface UserApi extends IService<User> {
-}
--- example/spring-cloud-example/provider/provider-user/src/main/java/com/guanmengyuan/spring/cloud/example/provider/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.guanmengyuan.spring.cloud.example.provider.mapper;
-
-import com.guanmengyuan.spring.cloud.example.model.domain.User;
-import com.mybatisflex.core.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
