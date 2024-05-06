ce9c03023b080db5dc841c0dd78e8a6354c46e1b
关梦园 <guan.mengyuan@icloud.com>
2024-05-06 15:31:50 +0800

    release to 1.2.47

--- spring-boot-starter-mybatis-flex/src/main/java/com/guanmengyuan/spring/ex/mybatis/flex/config/MyBatisFlexConfiguration.java
+++ spring-boot-starter-mybatis-flex/src/main/java/com/guanmengyuan/spring/ex/mybatis/flex/config/MyBatisFlexConfiguration.java
@@ -1,26 +1,15 @@
 package com.guanmengyuan.spring.ex.mybatis.flex.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.mybatisflex.core.logicdelete.LogicDeleteProcessor;
+import com.mybatisflex.core.logicdelete.impl.BooleanLogicDeleteProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.guanmengyuan.spring.ex.common.model.domain.BaseDomain;
-import com.guanmengyuan.spring.ex.common.mybatis.flex.listener.BaseInsertListener;
-import com.mybatisflex.core.FlexGlobalConfig;
-import com.mybatisflex.core.audit.AuditManager;
-import com.mybatisflex.core.logicdelete.LogicDeleteManager;
-import com.mybatisflex.core.logicdelete.impl.BooleanLogicDeleteProcessor;
-import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
-
 @Configuration
-@EnableConfigurationProperties(MyBatisFlexProperties.class)
-public class MyBatisFlexConfiguration implements MyBatisFlexCustomizer {
-    public MyBatisFlexConfiguration(MyBatisFlexProperties myBatisFlexProperties) {
-        AuditManager.setAuditEnable(myBatisFlexProperties.getAuditEnable());
-        LogicDeleteManager.setProcessor(new BooleanLogicDeleteProcessor());
-    }
+public class MyBatisFlexConfiguration {
 
-    @Override
-    public void customize(FlexGlobalConfig globalConfig) {
-        globalConfig.registerInsertListener(new BaseInsertListener(), BaseDomain.class);
+    @Bean
+    public LogicDeleteProcessor logicDeleteProcessor() {
+        return new BooleanLogicDeleteProcessor();
     }
 }
