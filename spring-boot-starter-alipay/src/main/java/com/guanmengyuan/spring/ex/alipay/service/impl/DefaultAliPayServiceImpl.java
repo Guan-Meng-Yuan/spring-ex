d66614984d2b0d03829041a361016bdcfaf502ca
关梦园 <guan.meng.yuan@icloud.com>
2024-10-31 17:38:18 +0800

    update  3.3.5.1

--- spring-boot-starter-alipay/src/main/java/com/guanmengyuan/spring/ex/alipay/service/impl/DefaultAliPayServiceImpl.java
+++ spring-boot-starter-alipay/src/main/java/com/guanmengyuan/spring/ex/alipay/service/impl/DefaultAliPayServiceImpl.java
@@ -1,31 +1,33 @@
 package com.guanmengyuan.spring.ex.alipay.service.impl;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alipay.api.AlipayApiException;
-import com.alipay.api.AlipayClient;
-import com.alipay.api.AlipayConfig;
-import com.alipay.api.DefaultAlipayClient;
-import com.alipay.api.request.AlipaySystemOauthTokenRequest;
-import com.alipay.api.response.AlipaySystemOauthTokenResponse;
-import com.alipay.easysdk.factory.MultipleFactory;
-import com.alipay.easysdk.kernel.Config;
-import com.guanmengyuan.spring.ex.alipay.config.AliPayProperties;
-import com.guanmengyuan.spring.ex.alipay.service.AliPayService;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.dromara.hutool.core.io.resource.ResourceUtil;
-
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.dromara.hutool.core.io.resource.ResourceUtil;
+import org.dromara.hutool.core.text.StrUtil;
+import org.dromara.hutool.core.util.CharsetUtil;
+import org.dromara.hutool.json.JSON;
+import org.dromara.hutool.json.JSONUtil;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.AlipayConfig;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipayEncrypt;
+import com.alipay.api.request.AlipaySystemOauthTokenRequest;
+import com.alipay.api.response.AlipaySystemOauthTokenResponse;
+import com.guanmengyuan.spring.ex.alipay.config.AliPayProperties;
+import com.guanmengyuan.spring.ex.alipay.service.AliPayService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+
 @RequiredArgsConstructor
 public class DefaultAliPayServiceImpl implements AliPayService {
     private static final Map<String, AlipayClient> clients = new ConcurrentHashMap<>();
     private static final Map<String, AlipayConfig> alipayConfigMap = new ConcurrentHashMap<>();
-    private static final Map<String, MultipleFactory> multipleFactoryMap = new ConcurrentHashMap<>();
 
     @Override
     public AlipayClient switchOverApiClient(String appId) {
@@ -40,32 +42,22 @@ public class DefaultAliPayServiceImpl implements AliPayService {
             AlipayConfig alipayConfig = new AlipayConfig();
             alipayConfig.setEncryptKey(appConfig.getEncryptKey());
             alipayConfig.setAppId(appId);
-            Config config = new Config();
-            config.appId = appId;
-            config.protocol = appConfig.getProtocol();
-            config.gatewayHost = appConfig.getGatewayHost();
-            config.signType = appConfig.getSignType();
-            config.encryptKey = appConfig.getEncryptKey();
 
             // 设置私钥
             String merchantPrivateKey = ResourceUtil.readStr(appConfig.getMerchantPrivateKeyPath(),
                     StandardCharsets.UTF_8);
 
-            config.merchantPrivateKey = merchantPrivateKey;
             alipayConfig.setPrivateKey(merchantPrivateKey);
 
             // 设置应用公钥证书文件
             String merchantCertPath = appConfig.getMerchantCertPath();
             alipayConfig.setAppCertContent(ResourceUtil.readUtf8Str(merchantCertPath));
-            config.merchantCertPath = merchantCertPath;
 
             // 设置支付宝公钥证书文件
             String alipayCertPath = appConfig.getAlipayCertPath();
-            config.alipayCertPath = alipayCertPath;
             alipayConfig.setAlipayPublicCertContent(ResourceUtil.readUtf8Str(alipayCertPath));
             // 支付宝根证书文件路径
             String alipayRootCertPath = appConfig.getAlipayRootCertPath();
-            config.alipayRootCertPath = alipayRootCertPath;
             alipayConfig.setRootCertContent(ResourceUtil.readUtf8Str(alipayRootCertPath));
             // 设置
             alipayConfig.setEncryptKey(appConfig.getEncryptKey());
@@ -76,14 +68,15 @@ public class DefaultAliPayServiceImpl implements AliPayService {
             } catch (AlipayApiException e) {
                 throw new RuntimeException(e);
             }
-            MultipleFactory multipleFactory = new MultipleFactory();
-            multipleFactory.setOptions(config);
-            multipleFactoryMap.put(appId, multipleFactory);
+
             alipayConfigMap.put(appId, alipayConfig);
             clients.put(appConfig.getAppId(), apiClient);
         });
     }
 
+    /**
+     * 获取openID
+     */
     @Override
     @SneakyThrows
     public String getOpenId(String authCode, String appId) {
@@ -98,16 +91,21 @@ public class DefaultAliPayServiceImpl implements AliPayService {
         return alipaySystemOauthTokenResponse.getOpenId();
     }
 
+    /**
+     * 获取手机号
+     */
     @SneakyThrows
     @Override
     public String getPhoneNumber(String phoneCode, String appId) {
-        String decrypted = multipleFactoryMap.get(appId).AES().decrypt(phoneCode);
-        JSONObject jsonObject = JSON.parseObject(decrypted);
-        if (!"10000".equals(jsonObject.get("code"))) {
-            throw new RuntimeException("数据解析失败");
+        AlipayConfig config = alipayConfigMap.get(appId);
+        JSON response = JSONUtil
+                .parse(AlipayEncrypt.decryptContent(phoneCode, config.getEncryptType(),
+                        config.getEncryptKey(), CharsetUtil.NAME_UTF_8));
+        if (StrUtil.equals(response.getByPath("code", String.class), "10000")
+                && StrUtil.equalsIgnoreCase(response.getByPath("msg", String.class), "Success")) {
+            return response.getByPath("mobile", String.class);
         }
-        Object mobile = jsonObject.get("mobile");
-        return mobile.toString();
+        throw new AlipayApiException("获取手机号失败");
     }
 
 }
